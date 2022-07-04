c2cb5e16ae0f8f62689f04c68ab3c545dd17b9cf
EstevanBecker <estevansaldanhadossantos@gmail.com>
2022-07-04 13:01:44 -0300

    Calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,7 @@
 
 const display = document.getElementById('display'); //Utilizando DOM para pegar o display
 
-const numero = document.querySelectorAll('[id*=botao]'); //Utilizando query para selecionar todos as ID que contenham number
+const numero = document.querySelectorAll('[id*=botao]'); //Utilizando query para selecionar todos as ID que contenham "botao"
 console.log(numero)
 
 const operadores = document.querySelectorAll('[id*=operador]');
